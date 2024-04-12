4589a6083123cec197a3c5209cc597a300e7a389
Kristian Kharl Tare <kristiankharl.tare@nmsc.edu.ph>
2024-04-12 11:20:24 +0800

    log in ui done

--- index.php
+++ index.php
@@ -14,8 +14,8 @@ $init->insertUniversityCourseReview(3,1,4,'Mana kog rate!');
 </head>
 
 <body>
-    <div class="w-screen h-screen ">
-        <div class="parent_container w-[70%] mx-auto bg-[#f6f6f9] h-full flex flex-col gap-20">
+    <div class="w-screen h-screen bg-green-50">
+        <div class="parent_container w-[70%] mx-auto bg-white h-full flex flex-col gap-20">
             <div class="header flex justify-between w-full items-start text-sm h-[5%]">
                 <div class="logo_container flex items-center justify-center gap-2 h-5"> <!-- Added flex and items-center for vertical alignment -->
                     <img src="imgs/logo.jpg" alt="" class="rounded-full w-10 h-10 object-cover mt-5"> <!-- Tailwind classes added here -->
